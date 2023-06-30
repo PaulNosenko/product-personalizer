2a670ad3ee51ef6338bed80d392b920b04d8b228
Paul Nosenko <pavelgadzenko@gmail.com>
2023-06-30 13:06:57 +0200

    [master] - Added chosing color and size functionality, and addToCart function

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,37 +2,70 @@ import styles from './Product.module.scss';
 import clsx from 'clsx';
 import Button from '../Button/Button';
 import PropTypes from 'prop-types'
+import { useState } from 'react';
 
 const Product = props => {
+  const [currentColor, setCurrentColor] = useState(props.colors[0]);
+  const [currentSize, setCurrentSize] = useState(props.sizes[0].name);
+
+  const prepareColorClassName = color => {
+    return styles['color' + color[0].toUpperCase() + color.substr(1).toLowerCase()];
+  }
+
+  const changeSize = (selectedSize) => {
+    setCurrentSize(selectedSize);
+  }
+
+  const changeColor = (selectedColor) => {
+    setCurrentColor(selectedColor);
+  }
+
+  const getPrice = () => {
+    return props.basePrice + props.sizes.find(s => s.name === currentSize).additionalPrice;
+  }
+
+  const addToCart = (e) => {
+    e.preventDefault();
+    console.group('Summary')
+    console.log('Name: ', props.title)
+    console.log('Price: ', getPrice())
+    console.log('Size: ', currentSize)
+    console.log('Color: ', currentColor)
+    console.groupEnd();
+  }
+
   return (
     <article className={styles.product}>
       <div className={styles.imageContainer}>
-        <img 
+        <img
           className={styles.image}
           alt="Kodilla shirt"
-          src={`${process.env.PUBLIC_URL}/images/products/shirt-kodilla--black.jpg`} />
+          src={`${process.env.PUBLIC_URL}/images/products/shirt-${props.name}--${currentColor}.jpg`} />
       </div>
       <div>
         <header>
           <h2 className={styles.name}>{props.title}</h2>
-          <span className={styles.price}>Price: {props.basePrice}$</span>
+          <span className={styles.price}>Price: {getPrice()}$</span>
         </header>
-        <form>
+        <form onSubmit={addToCart}>
           <div className={styles.sizes}>
             <h3 className={styles.optionLabel}>Sizes</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={styles.active}>S</button></li>
-              <li><button type="button">M</button></li>
-              <li><button type="button">L</button></li>
-              <li><button type="button">XL</button></li>
+              {props.sizes.map(s => {
+                return <li key={s.name}>
+                  <button type="button" onClick={changeSize.bind(null, s.name)} className={clsx(s.name === currentSize && styles.active)}>{s.name}</button>
+                </li>
+              })}
             </ul>
           </div>
           <div className={styles.colors}>
             <h3 className={styles.optionLabel}>Colors</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={clsx(styles.colorBlack, styles.active)} /></li>
-              <li><button type="button" className={clsx(styles.colorRed)} /></li>
-              <li><button type="button" className={clsx(styles.colorWhite)} /></li>
+              {props.colors.map(c => {
+                return <li key={c}>
+                  <button type="button" onClick={changeColor.bind(null, c)} className={clsx(prepareColorClassName(c), c === currentColor && styles.active)} />
+                </li>
+              })}
             </ul>
           </div>
           <Button className={styles.button}>
